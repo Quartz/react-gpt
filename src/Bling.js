a9d8cb5a4d32b1b20cf05af2bd5091d2cf87474c
Micah Ernst <micahwave@gmail.com>
2018-02-06 13:22:28 -0500

    Dont change correlator when refreshing, it should be handled elsewhere.

--- src/Bling.js
+++ src/Bling.js
@@ -431,7 +431,7 @@ class Bling extends Component {
 
         if (Bling._adManager._syncCorrelator) {
             if (shouldRefresh) {
-                this.refresh();
+                this.refresh({ changeCorrelator: false });
             } else if (shouldRender || isScriptLoaded) {
                 Bling._adManager.renderAll();
             }
